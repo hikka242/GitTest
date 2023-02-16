ac2c4bf090aec801dddb0e24fca73d78760ffc5a
hikka242 <glepol03@mail.ru>
2023-02-16 22:58:40 +0300

    added sout hi

--- src/GitTest/Test.java
+++ src/GitTest/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         TestGit t1=new TestGit();
         System.out.println(t1.gitTest());
+        System.out.println("hi");
     }
 
 }
